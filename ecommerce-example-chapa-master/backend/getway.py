7f74d96c24e6552dd2a9f92c2deb486da6d78951
fuadwa <fuadjemal35@gmail.com>
2024-07-25 02:36:56 +0300

    finishing backend with redirct url

--- ecommerce-example-chapa-master/backend/getway.py
+++ ecommerce-example-chapa-master/backend/getway.py
@@ -1,13 +1,16 @@
 from chapa import Chapa
+import time
+import requests
 from datetime import datetime
 
-key_api = 'ur_api_key'
+key_api = 'CHASECK_TEST-SLtpoGgR6Z0oiVoVnnNUK2wRJAa9ob5d'
 
+# Generate a unique transaction reference using the current timestamp
+unique_tx_ref = f"tx_fuad-{int(time.time())}"
+chapa_instance = Chapa(key_api)
 
-chapa = Chapa(key_api)
-
-
-class payment():
+class Payment:
+    @staticmethod
     def pay(email, fname, lname, amount, rdurl):
         now = datetime.now()
         tx_un_num = now.strftime("%m%d%Y%H%M%S")
@@ -18,19 +21,23 @@ class payment():
             'first_name': fname,
             'last_name': lname,
             'tx_ref': tx_id,
-            'return_url': rdurl,
+            'return_url': 'https://www.google.com',
+            'rdurl': rdurl,
             'customization': {
-                'title': '2utube',
-                'description': 'Payment for your services',
+                'title': 'utube',
+                'description': 'payment for your services',
             }
         }
 
-        response = chapa.initialize(**data)
+        response = chapa_instance.initialize(**data)
         return {'detail': response, 'tx_id': tx_id}
-        # print(response)
 
-    def verify(tx_num):
-        # # How to verify a transaction
-        response = chapa.verify(tx_num)
-        # print(response)
-        return response
+    @staticmethod
+    def custom_verify(transaction_ref):
+        url = f"https://api.chapa.co/v1/transaction/verify/{transaction_ref}"
+        headers = {
+            'Authorization': f'Bearer {key_api}',
+            'Content-Type': 'application/json',
+        }
+        response = requests.get(url, headers=headers)
+        return response.json()
--- ecommerce-example-chapa-master/backend/index.py
+++ ecommerce-example-chapa-master/backend/index.py
@@ -1,62 +1,42 @@
 import time
 import requests
-
 from fastapi import FastAPI
-from chapa import Chapa
 from pydantic import BaseModel
-
-
+from getway import Payment
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-key_api = 'CHASECK_TEST-SLtpoGgR6Z0oiVoVnnNUK2wRJAa9ob5d'
+origins = ["*"]
 
-#Generate a unique transaction reference using the current timestamp
-unique_tx_ref = f"tx_fuad-{int(time.time())}"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-data = {
-    'email': 'testwe1@utube.com',
-    'amount': 1000,
-    'first_name': 'testt',
-    'last_name': 'we1',
-    'tx_ref': unique_tx_ref,
-    'return_url': 'https://www.google.com',
-    'callback_url': 'https://www.google.com/callback',
-    'customization': {
-        'title': 'utube',
-        'description': 'payment for your services',
-    }
-}
-
-chapa_instance = Chapa(key_api)
-response = chapa_instance.initialize(**data)
-print("Initialization response:", response)
-
-# Manually verify the transaction
-def custom_verify(transaction_ref):
-    url = f"https://api.chapa.co/v1/transaction/verify/{transaction_ref}"
-    headers = {
-        'Authorization': f'Bearer {key_api}',
-        'Content-Type': 'application/json',
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
-
-verify_response = custom_verify(unique_tx_ref)
-print("Custom verification response:", verify_response)
-
-@app.get('/')
-def main():
-    return {"Hello": "fuad"}
-
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
 
 class Payit(BaseModel):
-    email:str='test@gmail.com'
-    fname:str='test'
-    lname:str='test'
-    amount:int=250
-
+    email: str = 'test@gmail.com'
+    fname: str = 'test'
+    lname: str = 'test'
+    amount: int = 250
+    rdurl: str = 'https://www.google.com'
 
 @app.post('/pay')
-def pay(payit:Payit):
-    return payit
+def pay(payit: Payit):
+    data = Payment.pay(email=payit.email, fname=payit.fname, lname=payit.lname, amount=payit.amount, rdurl=payit.rdurl)
+    return data
+
+class Txnum(BaseModel):
+    ref_num: str = ''
+
+@app.post('/ver')
+def verify(txnum: Txnum):
+    ver = Payment.custom_verify(transaction_ref=txnum.ref_num)
+    return ver
