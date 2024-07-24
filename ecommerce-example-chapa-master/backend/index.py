a5b981c23d950e70260e486b6ee26a11d12ee025
fuadwa <fuadjemal35@gmail.com>
2024-07-25 01:39:53 +0300

    route making with fastapi

--- ecommerce-example-chapa-master/backend/index.py
+++ ecommerce-example-chapa-master/backend/index.py
@@ -1,10 +1,17 @@
-import requests
 import time
+import requests
+
+from fastapi import FastAPI
 from chapa import Chapa
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
 
 key_api = 'CHASECK_TEST-SLtpoGgR6Z0oiVoVnnNUK2wRJAa9ob5d'
 
-# Generate a unique transaction reference using the current timestamp
+#Generate a unique transaction reference using the current timestamp
 unique_tx_ref = f"tx_fuad-{int(time.time())}"
 
 data = {
@@ -37,3 +44,19 @@ def custom_verify(transaction_ref):
 
 verify_response = custom_verify(unique_tx_ref)
 print("Custom verification response:", verify_response)
+
+@app.get('/')
+def main():
+    return {"Hello": "fuad"}
+
+
+class Payit(BaseModel):
+    email:str='test@gmail.com'
+    fname:str='test'
+    lname:str='test'
+    amount:int=250
+
+
+@app.post('/pay')
+def pay(payit:Payit):
+    return payit
