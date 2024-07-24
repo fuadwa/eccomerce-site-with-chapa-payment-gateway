3dd4aa9fd848f7d860036fe60700d389d5b707db
fuadwa <fuadjemal35@gmail.com>
2024-07-24 23:07:33 +0300

    payment integration with identifying suceess or not

--- ecommerce-example-chapa-master/backend/index.py
+++ ecommerce-example-chapa-master/backend/index.py
@@ -1,10 +1,11 @@
-from chapa import Chapa
+import requests
 import time
+from chapa import Chapa
 
 key_api = 'CHASECK_TEST-SLtpoGgR6Z0oiVoVnnNUK2wRJAa9ob5d'
 
 # Generate a unique transaction reference using the current timestamp
-unique_tx_ref = f"tx_abebebikilaa-{int(time.time())}"
+unique_tx_ref = f"tx_fuad-{int(time.time())}"
 
 data = {
     'email': 'testwe1@utube.com',
@@ -12,15 +13,27 @@ data = {
     'first_name': 'testt',
     'last_name': 'we1',
     'tx_ref': unique_tx_ref,
-    'return_url':'https://www.google.com',
-    'callback_url':'https://www.google.com/callback',
-    
-    'customization':{
-        'title':'utube',
-        'description':'payment for your services',
+    'return_url': 'https://www.google.com',
+    'callback_url': 'https://www.google.com/callback',
+    'customization': {
+        'title': 'utube',
+        'description': 'payment for your services',
     }
 }
 
 chapa_instance = Chapa(key_api)
 response = chapa_instance.initialize(**data)
-print(response)
+print("Initialization response:", response)
+
+# Manually verify the transaction
+def custom_verify(transaction_ref):
+    url = f"https://api.chapa.co/v1/transaction/verify/{transaction_ref}"
+    headers = {
+        'Authorization': f'Bearer {key_api}',
+        'Content-Type': 'application/json',
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+verify_response = custom_verify(unique_tx_ref)
+print("Custom verification response:", verify_response)
